e05ebfc1a594ee3cdd46962ca2ab18a9887b8e16
Jemil-B <jemil.burns@g.austincc.edu>
2024-09-14 20:05:00 +0000

    Homework 2

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,18 @@
-#
+#main get_options_ratio and get_faculty_rating
+
+import decisions
+
+def main():
+
+    option = int(input("Enter option: "))
+    total = int(input("Enter total: "))
+
+    try:
+        result1 = decisions.get_options_ratio(option, total)
+        result2 = decisions.get_faculty_rating(result1)
+        print("Ratio: ", result1)
+        print("Rating: ", result2)
+    except ZeroDivisionError:
+        print("Total cannot be 0")
+  
+main()
