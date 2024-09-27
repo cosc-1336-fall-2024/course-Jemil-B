ef4e8c989fae100eb21f5d135ee23fd6d8950999
Jemil-B <jemil.burns@g.austincc.edu>
2024-09-27 19:21:42 +0000

    hw4

--- src/examples/e_functions/value_return_functions.py
+++ src/examples/e_functions/value_return_functions.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 def test_config():
     return True
@@ -7,6 +8,14 @@ def echo_value(num):
     return num
 
 def generate_random_value(min_num, max_num):
-    random.seed()
     number = random.randint(min_num, max_num)
     return  number
+
+def get_sqrt(num):
+    return math.sqrt(num)
+
+
+def get_hours(epoch_seconds):
+    result = epoch_seconds  // 3600
+    remainder = result % 24
+    return remainder
--- tests/examples/e_functions/tests_functions.py
+++ tests/examples/e_functions/tests_functions.py
@@ -1,6 +1,7 @@
 import unittest
 
-from src.examples.e_functions.value_return_functions import test_config
+from src.examples.e_functions.value_return_functions import get_sqrt, test_config, generate_random_value
+from tests.homework.e_functions.tests_functions import get_hours
 
 class Test_Config(unittest.TestCase):
 
@@ -8,6 +9,16 @@ class Test_Config(unittest.TestCase):
         self.assertEqual(True, test_config())
 
 def test_get_random_number(self):
-    number = get_random_value(1,100)
-    self.assertEqual(True, (number >=1))
-    self.assertEqual(True, (number<=100))
+
+    for i in range(0, 10):
+        number = generate_random_value(1, 100)
+        self.assertEqual(True, (number >= 1))
+        self.assertEqual(True, (number <= 100))
+
+def test_get_sqrt(self):
+    self.assertEqual(3, get_sqrt(9))
+    self.assertEqual(4, get_sqrt(16))
+    self.assertEqual(5, get_sqrt(25))
+
+def test_get_hours_epoch(self):
+    self.assertEqual(22,get_hours(1727390328))
